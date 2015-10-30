2e26aa6f788857bb62310eb2206c597c2525b181
brubom@gmail.com <Nevasca13@>
2015-10-30 16:05:57 -0400

    first version

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,54 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+
+  return arr.inject { |n, y|  n + y}
+
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.size == 1
+
+  #return arr.each { |n,y| n<=>y }.first(2).inject { |n, y|  n + y}
+
+  sorted = arr.sort
+  return sorted[-1] + sorted[-2]
+
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+
+  return false if arr.empty?
+  return false if arr.size == 1
+
+  result = false
+
+  arr.each_slice(2).each { |x|
+    result = x.inject { |v, y|  v + y} == n
+    break if result
+  }
+  result
+
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+
+  result = !!(s.downcase =~ /\A[aeiou]/)
+  return !result
+
+
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  s =~ /^[01]*1[01]*00$/
 end
 
 # Part 3
